b7831e882c6a29f766d4a36526006e75c466ef82
Maxime <rivalin.maxime@gmail.com>
2014-12-04 09:23:16 +0100

    Infos sur les fonctions

--- Outils/PileDynamiqueGenerique.c
+++ Outils/PileDynamiqueGenerique.c
@@ -3,32 +3,46 @@
 #include <string.h>
 #include "PileDynamiqueGenerique.h"
 
-
+/*
+ init_pile :
+ cette fonction permet d'initialiser une pile dynamique générique.
+ 
+ entrée :
+ - size_t : taille de la donnée que l'on va stocker
+ 
+ sortie :
+ _ Pile : pile générique
+ */
 void init_pile(Pile* p, size_t s){
     p->type = s;
     p->taille = 0;
     p->tete = NULL;
 }
 
-void afficher_pile(const Pile* p){
-    Cellule* tmp = (p->tete);
-    int e;
-    
-    printf("[ ");
-    if (!pile_est_vide(p)){
-        while (tmp != NULL){
-            e=(int)tmp->info;
-            printf("%d ", e);
-            tmp = tmp->suiv;
-        }
-    }
-    printf("]\n");
-}
-
+/*
+ pile_est_vide :
+ cette fonction permet de savoir si la pile est vide ou non.
+ 
+ entée :
+ - Pile : pile dont on veut savoir si elle est vide
+ 
+ retourne 1 si la pile est vide, 0 sinon
+ */
 int pile_est_vide(const Pile* p){
     return p->taille == 0;
 }
 
+/*
+ empiler :
+ cette fonction qui permet d'empiler un élément.
+ 
+ entrées :
+ - void* : adresse de l'élément à empiler
+ - Pile* : adresse de la pile
+ 
+ sorties :
+ - Pile* : adresse de la pile avec le nouvel élément
+ */
 void empiler(Pile* p, void* e){
     Cellule *tmp;
     
@@ -39,11 +53,24 @@ void empiler(Pile* p, void* e){
     p->taille++;
 }
 
+/*
+ depiler :
+ cette fonction qui permet depiler un élément.
+ 
+ entrées :
+ - void* : adresse de l'élément dépilé
+ - Pile* : adresse de la pile
+ 
+ sorties :
+ - Pile* : adresse de la pile avec l'élément dépilé
+ 
+ retourne 0 si la fonction dipiler n'a pas fonctionnée, 1 sinon
+ */
 int depiler(Pile* p, void* e){
     Cellule* tmp;
     
     if (pile_est_vide(p))
-        return EXIT_FAILURE;
+        return 0;
     
     tmp = p->tete;
     memcpy(e, &(p->tete->info), p->type);
@@ -51,9 +78,18 @@ int depiler(Pile* p, void* e){
     free(tmp);
     p->taille--;
     
-    return EXIT_SUCCESS;
+    return 1;
 }
 
+/*
+ taille_pile :
+ cette fonction retourne la taille de la pile passée en paramètre.
+ 
+ entrée :
+ - Pile : pile dont on veut connaitre la taille
+ 
+ retourne la taille de la pile
+ */
 int taille_pile(const Pile* p){
     return p->taille;
 }
--- Outils/PileDynamiqueGenerique.h
+++ Outils/PileDynamiqueGenerique.h
@@ -1,19 +1,77 @@
-#ifndef __file_generique__PileDynamiqueGenerique__
-#define __file_generique__PileDynamiqueGenerique__
+#ifndef __Outils__PileDynamiqueGenerique__
+#define __Outils__PileDynamiqueGenerique__
 
 #include "Cellule.h"
 
+/*
+ Structure de la pile.
+ */
 typedef struct {
     size_t type;
     int taille;
     Cellule* tete;
 } Pile;
 
+/*
+ init_pile :
+ cette fonction permet d'initialiser une pile dynamique générique.
+ 
+ entrée :
+ - size_t : taille de la donnée que l'on va stocker
+ 
+ sortie :
+ _ Pile : pile générique
+ */
 void init_pile (Pile* p, size_t s);
-void afficher_pile (const Pile* p);
+
+/*
+ pile_est_vide :
+ cette fonction permet de savoir si la pile est vide ou non.
+ 
+ entée :
+ - Pile : pile dont on veut savoir si elle est vide
+ 
+ retourne 1 si la pile est vide, 0 sinon
+ */
 int pile_est_vide (const Pile* p);
+
+/*
+ taille_pile :
+ cette fonction retourne la taille de la pile passée en paramètre.
+ 
+ entrée :
+ - Pile : pile dont on veut connaitre la taille
+ 
+ retourne la taille de la pile
+ */
 int taille_pile(const Pile* p);
+
+/*
+ empiler :
+ cette fonction qui permet d'empiler un élément.
+ 
+ entrées :
+ - void* : adresse de l'élément à empiler
+ - Pile* : adresse de la pile
+ 
+ sorties :
+ - Pile* : adresse de la pile avec le nouvel élément
+ */
 void empiler (Pile* p,void* e);
+
+/*
+ depiler :
+ cette fonction qui permet depiler un élément.
+ 
+ entrées :
+ - void* : adresse de l'élément dépilé
+ - Pile* : adresse de la pile
+ 
+ sorties :
+ - Pile* : adresse de la pile avec l'élément dépilé
+ 
+ retourne 0 si la fonction dipiler n'a pas fonctionnée, 1 sinon
+ */
 int depiler (Pile* p, void* e);
 
 #endif
